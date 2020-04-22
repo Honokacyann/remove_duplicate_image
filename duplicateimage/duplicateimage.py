e8f408c848a5c2fd051a28ec5a8519303a86fbad
honokacyann <1915569481@qq.com>
2020-04-22 17:06:43 +0800

    去除重复图片主代码

--- duplicateimage/duplicateimage.py
+++ duplicateimage/duplicateimage.py
@@ -1,1 +1,9 @@
+import os
+from imagededup.methods import PHash
 
+if __name__ == '__main__':
+    phasher = PHash()
+    duplicates_list = phasher.find_duplicates_to_remove('I://duplicatepic//001')
+    for i in range(0,len(duplicates_list)-1):
+        duplicates_list_remove = 'I://duplicatepic//001//'+duplicates_list[i]
+        os.remove(duplicates_list_remove)
