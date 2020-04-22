d62fc96b03e2711c1e50586eef638a82dd41372a
honokacyann <1915569481@qq.com>
2020-04-22 17:15:30 +0800

    全局路径名

--- duplicateimage/duplicateimage.py
+++ duplicateimage/duplicateimage.py
@@ -1,9 +1,10 @@
 import os
 from imagededup.methods import PHash
+image_dir ='I://net hat//001'
 
 if __name__ == '__main__':
     phasher = PHash()
-    duplicates_list = phasher.find_duplicates_to_remove('I://duplicatepic//001')
+    duplicates_list = phasher.find_duplicates_to_remove(image_dir)
     for i in range(0,len(duplicates_list)-1):
-        duplicates_list_remove = 'I://duplicatepic//001//'+duplicates_list[i]
+        duplicates_list_remove = image_dir + '//' + duplicates_list[i]
         os.remove(duplicates_list_remove)
